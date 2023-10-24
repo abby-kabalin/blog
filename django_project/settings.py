637d3db8056735d0a4c6ca2b13ace90ccc86dbbb
Abby Kabalin <ackabalin@yahoo.com>
2023-10-23 19:21:38 -0600

    fly missed installations remidied

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [".fly.dev", "localhose", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
